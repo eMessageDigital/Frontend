f6f3ff81dd4c2648ddf75e88f0982212b2c0b693
Ruslan <otmena163@gmail.com>
2025-09-24 01:15:27 +0400

    Add responsive HomePage

--- app/core/components/modules/Header/Header.tsx
+++ app/core/components/modules/Header/Header.tsx
@@ -1,32 +1,56 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./Header.module.scss";
 
 import Image from "next/image";
-import Link from "next/link";
 import { Button, Container, HoverLink } from "../..";
 
 export const Header = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
-		<header className={styles.headerWrapper}>
-			<Container className={styles.header}>
+		<Container>
+			<header className={styles.header}>
 				<div className={styles["logo-nav"]}>
 					<div className={styles.logo}>
 						<Image src='/img/logo.png' alt='Logo' width={42} height={42} />
 					</div>
 
-					<nav className={styles.nav}>
+					{/* Гамбургер */}
+					<button className={styles.burger} onClick={() => setMenuOpen(true)}>
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+
+					{/* Навигация */}
+					<nav className={`${styles.nav} ${menuOpen ? styles.active : ""}`}>
+						{/* Кнопка закрытия */}
+						<button className={styles.close} onClick={() => setMenuOpen(false)}>
+							×
+						</button>
+
 						<HoverLink href='/'>Главная</HoverLink>
 						<HoverLink href='/services'>Услуги</HoverLink>
 						<HoverLink href='#'>Кейсы</HoverLink>
 						<HoverLink href='/faq'>FAQ</HoverLink>
 						<HoverLink href='#'>Контакты</HoverLink>
+
+						{/* Мобильные кнопки */}
+						<div className={styles.mobileButtons}>
+							<Button className={styles.login}>Вход</Button>
+							<Button className={styles.signin}>Регистрация</Button>
+						</div>
 					</nav>
 				</div>
 
+				{/* Десктопные кнопки */}
 				<div className={styles.buttons}>
 					<Button className={styles.login}>Вход</Button>
 					<Button className={styles.signin}>Регистрация</Button>
 				</div>
-			</Container>
-		</header>
+			</header>
+		</Container>
 	);
 };
