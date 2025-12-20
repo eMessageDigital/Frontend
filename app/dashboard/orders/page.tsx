f9552e96413cba869512f9bcf4ee8e3a7ff8a990
Ruslan Timergaliev <otmena163@gmail.com>
2025-12-20 19:06:39 +0400

    Fix

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -2,8 +2,7 @@
 
 import React from "react";
 import { Orders } from "../../core/components";
-import { useOrders } from "../../core/components/backend/hooks";
 
 export default function page() {
-	return <Orders useOrdersHook={useOrders} title='История заказов' />;
+	return <Orders title='История заказов' />;
 }
